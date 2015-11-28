937f227d10b79173418d1ea4f88bb1ee71b2c877
Nikolay Mateev <bgdunkers@gmail.com>
2015-11-28 14:15:45 -0800

    Week 2 day 1

---
+++ Week 2/01-Methods/DirectoryTraversal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Directory_traversal
+{
+    class DirectoryTraversal
+    {
+        static IEnumerable<string> IterateDirectory(DirectoryInfo dir)
+        {
+            foreach (var file in dir.GetFiles("*", SearchOption.AllDirectories))
+            {
+                yield return file.FullName;
+            }
+        }
+
+        static void Main()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Nicky\Desktop\Directory");
+            string str = string.Join(Environment.NewLine, IterateDirectory(dir));
+            Console.WriteLine(str);
+        }
+    }
+}
