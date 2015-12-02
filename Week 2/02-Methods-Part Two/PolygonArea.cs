010ca2f0c2bb1c0b6c46ca54cf13d50404836cbe
Nikolay Mateev <bgdunkers@gmail.com>
2015-12-02 00:19:01 -0800

    Week 2, day 2

---
+++ Week 2/02-Methods-Part Two/PolygonArea.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace Polygon_Area
+{
+    class PolygonArea
+    {
+        static float CalcArea(PointF[] points)
+        {
+            float area = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (i != points.Length - 1)
+                {
+                    area += (points[i].X * points[i + 1].Y) - (points[i + 1].X * points[i].Y);
+                }
+                else
+                {
+                    area += (points[i].X * points[0].Y) - (points[i].Y * points[0].X);
+                }
+            }
+            return area;
+        }
+
+        static void Main()
+        {
+            PointF[] points = new PointF[]
+            {
+                new PointF(1, 0),
+                new PointF(1, 1),
+                new PointF(0, 1),
+                new PointF(0, 0)
+            };
+            Console.WriteLine("Area: {0}", CalcArea(points));
+        }
+    }
+}
---
+++ Week 2/02-Methods-Part Two/PolygonCircumference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace Polygon_Circumference
+{
+    class PolygonCircumference
+    {
+        static float CalcCircumference(PointF[] points)
+        {
+            float sum = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (i != points.Length - 1)
+                {
+                    sum += (float)Math.Sqrt(Math.Pow(points[i + 1].X - points[i].X, 2) + Math.Pow(points[i + 1].Y - points[i].Y, 2));
+                }
+                else
+                {
+                    sum += (float)Math.Sqrt(Math.Pow(points[i].X - points[0].X, 2) + Math.Pow(points[i].Y - points[0].Y, 2));
+                }
+            }
+            return sum;
+        }
+
+        static void Main()
+        {
+            PointF[] points = new PointF[]
+            {
+                new PointF(1, 0),
+                new PointF(1, 1),
+                new PointF(0, 1),
+                new PointF(0, 0)
+            };
+
+            Console.WriteLine("Circumference: {0}", CalcCircumference(points));
+        }
+    }
+}
