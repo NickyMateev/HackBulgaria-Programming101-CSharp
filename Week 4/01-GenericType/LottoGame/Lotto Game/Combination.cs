cebb197decff634f6e004de2ff4f24d48def6cf7
Nikolay Mateev <bgdunkers@gmail.com>
2015-12-23 23:17:26 +0200

    Week 4, day 1

---
+++ Week 4/01-GenericType/LottoGame/Lotto Game/Combination.cs
@@ -0,0 +1,56 @@
+﻿namespace Lotto_Game
+{
+    public class Combination<T, U>
+    {
+        private T[] firstHalfValues;
+        private U[] secondHalfValues;
+
+        public Combination(T[] firstHalfValues, U[] secondHalfValues)
+        {
+            this.firstHalfValues = firstHalfValues;
+            this.secondHalfValues = secondHalfValues;
+            FirstHalf = firstHalfValues;
+            SecondHalf = secondHalfValues;
+        }
+
+        public T[] FirstHalf { get; }
+        public U[] SecondHalf { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Combination<T, U>)
+            {
+                Combination<T, U> obj1 = this;
+                Combination<T, U> obj2 = (Combination<T, U>)obj;
+
+                for (int i = 0; i < obj1.FirstHalf.Length; i++)
+                {
+                    if (!obj1.firstHalfValues[i].Equals(obj2.firstHalfValues[i]))
+                        return false;
+                    if (!obj1.secondHalfValues[i].Equals(obj2.secondHalfValues[i]))
+                        return false;
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + firstHalfValues[0].GetHashCode();
+                hash = hash * 23 + firstHalfValues[1].GetHashCode();
+                hash = hash * 23 + firstHalfValues[2].GetHashCode();
+                hash = hash * 42 + secondHalfValues[0].GetHashCode();
+                hash = hash * 42 + secondHalfValues[1].GetHashCode();
+                hash = hash * 42 + secondHalfValues[2].GetHashCode();
+                return hash;
+            }
+        }
+
+    }
+}
---
+++ Week 4/01-GenericType/LottoGame/Lotto Game/LottoResult.cs
@@ -0,0 +1,30 @@
+﻿namespace Lotto_Game
+{
+    public class LottoResult<T, U>
+    {
+        public LottoResult()
+        {
+            IsWinning = false;
+            MatchedNumbersCount = 0;
+        }
+
+        public bool IsWinning { get; }
+        public byte MatchedNumbersCount { get; }
+
+        public LottoResult(Combination<T, U> combination, Combination<T, U> winningCombination)
+        {
+            MatchedNumbersCount = 0;
+            for (int i = 0; i < combination.FirstHalf.Length; i++)
+            {
+                if (combination.FirstHalf[i].Equals(winningCombination.FirstHalf[i]))
+                    MatchedNumbersCount++;
+                if (combination.SecondHalf[i].Equals(winningCombination.SecondHalf[i]))
+                    MatchedNumbersCount++;
+            }
+
+            if (MatchedNumbersCount > 1)
+                IsWinning = true;
+        }
+
+    }
+}
