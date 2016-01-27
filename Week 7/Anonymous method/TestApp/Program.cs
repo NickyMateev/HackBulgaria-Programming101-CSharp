492b1017259f56e0696e30eca1a59f7f4b56955c
Nikolay Mateev <bgdunkers@gmail.com>
2016-01-27 15:19:21 +0200

    Week 7, day 1

---
+++ Week 7/Anonymous method/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AnonymousAggregatorMethod;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            AggregatorMethod aggregator = new AggregatorMethod();
+            aggregator.AverageChanged += delegate (object sender, EventArgs e)
+            {
+                AggregatorMethod agg = (AggregatorMethod)sender;
+                Console.WriteLine("Average changed!\nNew average: {0}", agg.Average);
+            };
+
+            aggregator.AddNumber(10);
+            aggregator.AddNumber(3);
+        }
+    }
+}
