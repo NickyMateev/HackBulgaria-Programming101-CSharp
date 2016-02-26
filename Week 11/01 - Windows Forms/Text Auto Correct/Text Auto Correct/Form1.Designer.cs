4af1ea4aefc5c6ef7e6fbee3836bb3014e607a85
Nikolay Mateev <bgdunkers@gmail.com>
2016-02-27 01:47:08 +0200

    Week 11, day 1

---
+++ Week 11/01 - Windows Forms/Text Auto Correct/Text Auto Correct/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Text_Auto_Correct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(230, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text Auto Correct";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(726, 229);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.textChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(663, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(582, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.clearTextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(750, 326);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Week 11/01 - Windows Forms/Text Auto Correct/Text Auto Correct/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Auto_Correct
+{
+    public partial class Form1 : Form
+    {
+        private List<string> dictionary = File.ReadLines(@"C:\Users\Nicky\Desktop\wordDict.txt").ToList();
+
+        private StringBuilder currentWord = new StringBuilder();
+        private bool inWord = false;
+        private int wordStartIndex = -1; // -1 will be the default value
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength == 0)
+            {
+                wordStartIndex = -1;
+                currentWord.Clear();
+                return;
+            }
+
+            if (richTextBox1.Text[richTextBox1.TextLength - 1] != ' ')
+            {
+                if (wordStartIndex == -1)
+                    wordStartIndex = richTextBox1.TextLength - 1;
+
+                currentWord.Append(richTextBox1.Text[richTextBox1.TextLength - 1]);
+                inWord = true;
+            }
+            else
+            {
+                if (inWord)
+                {
+                    char lastChar = currentWord.ToString()[currentWord.Length - 1];
+                    while (lastChar == '.' || lastChar == ',' || lastChar == ';' || lastChar == '!' || lastChar == '?')
+                        currentWord.Remove(currentWord.Length - 1, 1);  // removing any punctuation marks
+
+                    AutoCorrectWord(currentWord.ToString().ToLower());    // checking for the word in the dictionary
+                }
+                    
+
+                inWord = false;
+                wordStartIndex = -1;
+            }
+
+        }
+
+        private void AutoCorrectWord(string wordToCheck)
+        {
+            if (!dictionary.Contains(wordToCheck))
+            {
+                double commonLetters = 0, curCommonRatio = 0, bestCommonRatio = 0;
+                string closestWord = string.Empty;
+                foreach (var word in dictionary)
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (wordToCheck.Length <= i)
+                            break;
+                        if (word[i] == wordToCheck[i])
+                            commonLetters++;
+                    }
+                    curCommonRatio = commonLetters / wordToCheck.Length;
+                    if (curCommonRatio > bestCommonRatio)
+                    {
+                        closestWord = word;
+                        bestCommonRatio = curCommonRatio;
+                    }
+                    commonLetters = 0;
+                }
+
+                richTextBox1.Text = richTextBox1.Text.Remove(wordStartIndex, richTextBox1.TextLength - wordStartIndex);
+                richTextBox1.AppendText(closestWord);
+            }
+           
+            currentWord.Clear();
+        }
+
+        private void clearTextButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.ResetText();
+
+            wordStartIndex = -1;
+            currentWord.Clear();
+        }
+    }
+}
